8f57cad551b8cc91b3f5b7847c4fbf38f3ab2888
Zach Pinfold <zach-pinfold@zachpinfold.home>
2023-09-16 10:48:32 +0100

    change port

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express"
 
 const app = express()
 
-app.listen(5001, () => console.log("running on port 5001"))
+app.listen(80, () => console.log("running on port 80"))
 
 app.get("/", (req, res) => res.json("My API running :)"))
