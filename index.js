fb602fa303eace94f16ffb8dba3d6330ef898bd3
Zach Pinfold <zach-pinfold@zachpinfold.home>
2023-12-13 19:49:35 +0000

    pewdpid

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(8080, () => console.log("runniwedng on port 8080"));
+app.listen(8080, () => console.log("runniwedng on port 808e0"));
 
 app.get("/", (req, res) => res.json("My API running :)"));
