beb27b0957b6ee74390bf6b7df17abc52e7f9d19
Zach Pinfold <zach-pinfold@zachpinfold.home>
2023-12-13 20:26:03 +0000

    send back html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.listen(8080, () => console.log("Five things I love about this place"));
+app.listen(8080, () => console.log("running on port 8080"));
 
-app.get("/", (req, res) => res.json("My API running :)"));
+app.get("/", (req, res) =>
+  res.send("<h1>Five things I love about this place</h1>")
+);
