7d2a6c8026d34ba887ff81711258427ce1dc2dfa
Zach Pinfold <zach-pinfold@zachpinfold.home>
2023-12-13 19:44:43 +0000

    small change

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.listen(8080, () => console.log("running on port 80080"));
+app.listen(8080, () => console.log("running on port 8080"));
 
 app.get("/", (req, res) => res.json("My API running :)"));
